8e560b382f7d05d39b37576b01ea9bed07aeb1d3
heymendezz <64676693+heymendezz@users.noreply.github.com>
2022-12-14 19:19:25 -0300

    base midi

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();  
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador)
+}
